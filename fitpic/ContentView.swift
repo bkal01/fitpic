4179040ca80d3d644d5a84790fd763c9ee30e261
Bhavesh Kalisetti <bhaveshkalisetti@Bhaveshs-MBP.attlocal.net>
2023-07-14 19:42:17 -0700

    initial commit

---
+++ fitpic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fitpic
+//
+//  Created by Bhavesh Kalisetti on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fitpic/fitpicApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitpicApp.swift
+//  fitpic
+//
+//  Created by Bhavesh Kalisetti on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct fitpicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
